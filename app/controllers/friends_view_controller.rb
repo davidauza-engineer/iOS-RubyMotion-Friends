7d6da6bf964bc8763729c4d0d460cc28a7aea835
davidauza.engineer <davidauza01@gmail.com>
2021-01-14 08:50:46 -0500

    Add table view to friends_view_controller

--- app/controllers/friends_view_controller.rb
+++ app/controllers/friends_view_controller.rb
@@ -9,14 +9,36 @@ class FriendsViewController < UIViewController
                                                           action: 'refresh_friends')
 
     self.navigationItem.rightBarButtonItem = refresh_button
+
+    view.delegate = self
+    view.dataSource = self
   end
 
   def loadView
     super
+    self.view = loadFriendsTableView
     view.backgroundColor = UIColor.whiteColor
+  end
+
+  def loadFriendsTableView
+    table_view = UITableView.alloc.initWithFrame(self.view.frame, style: UITableViewStylePlain)
+    table_view.autoresizingMask = UIViewAutoresizingFlexibleHeight
+    table_view
   end
 
   def refresh_friends
     puts 'Actualizar'
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    4
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @cell_id ||= 'CELL_IDENTIFIER'
+    cell = tableView.dequeueReusableCellWithIdentifier(@cell_id)
+    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @cell_id)
+    cell.textLabel.text = 'Hi'
+    cell
   end
 end
